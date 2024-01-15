f88046cde9ebd65253b2ef0627b55efec184033d
Benji Dudu <benjamin.duhieu@epitech.eu>
2024-01-15 15:27:53 +0100

    feat: Reorganize repository to manage local data

---
+++ JambleTest/Data/Services/Network/Products/Resource+Products.swift
@@ -0,0 +1,8 @@
+//
+//  Resource+Products.swift
+//  JambleTest
+//
+//  Created by Benjamin Duhieu on 15/01/2024.
+//
+
+import Foundation
---
+++ JambleTest/UI/Product/ProductsCellDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  ProductsCellDelegate.swift
+//  JambleTest
+//
+//  Created by Benjamin Duhieu on 15/01/2024.
+//
+
+import Foundation
